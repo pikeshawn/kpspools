7b38895fe848cf493a8500581817fd1913647c7c
Shawn Pike <pike.shawn@gmail.com>
2022-06-03 16:53:10 -0700

    updated the notes again

--- resources/js/Pages/ServiceStops/Notes.vue
+++ resources/js/Pages/ServiceStops/Notes.vue
@@ -8,25 +8,26 @@
         <br>
 
 
-        <ul role="list" class="divide-y divide-gray-200">
-            <li v-for="note in notes" :key="note.id" class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-                <div class="flex justify-between space-x-3">
-                    <div class="min-w-0 flex-1">
-                        <a href="#" class="block focus:outline-none">
-                            <span class="absolute inset-0" aria-hidden="true" />
-                            <p class="text-sm font-medium text-gray-900 truncate">{{ note.service_type }}</p>
-<!--                            <p class="text-sm text-gray-500 truncate">{{ note.notes }}</p>-->
-                        </a>
+        <div role="list" class="divide-y divide-gray-200">
+            <div v-for="note in noNullNotes" :key="note.id" class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                <div v-if="note.notes">
+                    <div class="flex justify-between space-x-3">
+                        <div class="min-w-0 flex-1">
+                            <a href="#" class="block focus:outline-none">
+                                <span class="absolute inset-0" aria-hidden="true" />
+                                <p class="text-sm font-medium text-gray-900 truncate font-extrabold">{{ note.service_type }}</p>
+                            </a>
+                        </div>
+                        <time :datetime="note.updated_at" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ note.updated_at }}</time>
                     </div>
-                    <time :datetime="note.updated_at" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ note.updated_at }}</time>
+                    <div class="mt-1">
+                        <p class="line-clamp-2 text-sm text-gray-600">
+                            {{ note.notes }}
+                        </p>
+                    </div>
                 </div>
-                <div class="mt-1">
-                    <p class="line-clamp-2 text-sm text-gray-600">
-                        {{ note.notes }}
-                    </p>
-                </div>
-            </li>
-        </ul>
+            </div>
+        </div>
 
     </layout>
 </template>
@@ -46,6 +47,19 @@ export default {
     props: {
         notes: Array,
         customer_name: String
+    },
+    computed: {
+        noNullNotes(){
+            let notes = []
+
+            for (let i = 0; i < this.notes.length; i++) {
+                if (this.notes[i].notes) {
+                    notes.push(this.notes[i])
+                }
+            }
+
+            return notes
+        }
     }
 }
 </script>
