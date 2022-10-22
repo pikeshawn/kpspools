4cdaea35cc98c7f87c07c0421094bf45b992144b
Shawn Pike <pike.shawn@gmail.com>
2022-10-22 16:53:20 -0700

    removed tom cook profile

--- resources/js/Pages/Shared/Layout.vue
+++ resources/js/Pages/Shared/Layout.vue
@@ -37,23 +37,23 @@
                                 <!--                                </a>-->
                             </nav>
                         </div>
-                        <div class="flex-shrink-0 flex bg-gray-700 p-4">
-                            <a href="#" class="flex-shrink-0 group block">
-                                <div class="flex items-center">
-                                    <div>
-                                        <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                    </div>
-                                    <div class="ml-3">
-                                        <p class="text-base font-medium text-white">
-                                            Tom Cook
-                                        </p>
-                                        <p class="text-sm font-medium text-gray-400 group-hover:text-gray-300">
-                                            View profile
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+<!--                        <div class="flex-shrink-0 flex bg-gray-700 p-4">-->
+<!--                            <a href="#" class="flex-shrink-0 group block">-->
+<!--                                <div class="flex items-center">-->
+<!--                                    <div>-->
+<!--                                        <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />-->
+<!--                                    </div>-->
+<!--                                    <div class="ml-3">-->
+<!--                                        <p class="text-base font-medium text-white">-->
+<!--                                            Tom Cook-->
+<!--                                        </p>-->
+<!--                                        <p class="text-sm font-medium text-gray-400 group-hover:text-gray-300">-->
+<!--                                            View profile-->
+<!--                                        </p>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </a>-->
+<!--                        </div>-->
                     </div>
                 </TransitionChild>
                 <div class="flex-shrink-0 w-14">
@@ -87,23 +87,23 @@
                             <!--                            </a>-->
                         </nav>
                     </div>
-                    <div class="flex-shrink-0 flex bg-gray-700 p-4">
-                        <a href="#" class="flex-shrink-0 w-full group block">
-                            <div class="flex items-center">
-                                <div>
-                                    <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                </div>
-                                <div class="ml-3">
-                                    <p class="text-sm font-medium text-white">
-                                        Tom Cook
-                                    </p>
-                                    <p class="text-xs font-medium text-gray-300 group-hover:text-gray-200">
-                                        View profile
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+<!--                    <div class="flex-shrink-0 flex bg-gray-700 p-4">-->
+<!--                        <a href="#" class="flex-shrink-0 w-full group block">-->
+<!--                            <div class="flex items-center">-->
+<!--                                <div>-->
+<!--                                    <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />-->
+<!--                                </div>-->
+<!--                                <div class="ml-3">-->
+<!--                                    <p class="text-sm font-medium text-white">-->
+<!--                                        Tom Cook-->
+<!--                                    </p>-->
+<!--                                    <p class="text-xs font-medium text-gray-300 group-hover:text-gray-200">-->
+<!--                                        View profile-->
+<!--                                    </p>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                        </a>-->
+<!--                    </div>-->
                 </div>
             </div>
         </div>
