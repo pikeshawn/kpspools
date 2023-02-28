85514f371913ec6405b7a7dbad85f47c96a50444
Shawn Pike <pike.shawn@gmail.com>
2023-02-28 07:10:21 -0700

    updated the home page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -25,52 +25,8 @@
                             <img class="w-auto w-2/5" src="/img/Original.png" alt="" />
                         </a>
                     </div>
-<!--                    <div class="-mr-2 -my-2 md:hidden">-->
-<!--                        <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">-->
-<!--                            <span class="sr-only">Open menu</span>-->
-<!--                            <MenuIcon class="h-6 w-6" aria-hidden="true" />-->
-<!--                        </PopoverButton>-->
-<!--                    </div>-->
-<!--                    <PopoverGroup as="nav" class="hidden md:flex space-x-10">-->
-<!--                        <Popover class="relative" v-slot="{ open }">-->
-<!--                            <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">-->
-<!--                                <span>Solutions</span>-->
-<!--                                <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />-->
-<!--                            </PopoverButton>-->
-
-<!--                            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">-->
-<!--                                <PopoverPanel class="absolute z-10 -ml-4 mt-3 transform w-screen max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">-->
-<!--                                    <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">-->
-<!--                                        <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">-->
-<!--                                            <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">-->
-<!--                                                <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white sm:h-12 sm:w-12">-->
-<!--                                                    <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />-->
-<!--                                                </div>-->
-<!--                                                <div class="ml-4">-->
-<!--                                                    <p class="text-base font-medium text-gray-900">-->
-<!--                                                        {{ item.name }}-->
-<!--                                                    </p>-->
-<!--                                                    <p class="mt-1 text-sm text-gray-500">-->
-<!--                                                        {{ item.description }}-->
-<!--                                                    </p>-->
-<!--                                                </div>-->
-<!--                                            </a>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </PopoverPanel>-->
-<!--                            </transition>-->
-<!--                        </Popover>-->
-
-<!--                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">-->
-<!--                            Pricing-->
-<!--                        </a>-->
-<!--                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">-->
-<!--                            Partners-->
-<!--                        </a>-->
-<!--                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">-->
-<!--                            Company-->
-<!--                        </a>-->
-<!--                    </PopoverGroup>-->
+                    <div class="-mr-2 -my-2 md:hidden">
+                    </div>
                     <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
 
 
@@ -169,64 +125,24 @@
             </Popover>
         </header>
 
-<!--        <main>-->
-<!--            &lt;!&ndash; Hero section &ndash;&gt;-->
-<!--            <div class="relative">-->
-<!--                <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />-->
-<!--                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">-->
-<!--                    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">-->
-<!--                        <div class="absolute inset-0">-->
-<!--                            <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100" alt="People working on laptops" />-->
-<!--                            <div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply" />-->
-<!--                        </div>-->
-<!--                        <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">-->
-<!--                            <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">-->
-<!--                                <span class="block text-white">Take control of your</span>-->
-<!--                                <span class="block text-indigo-200">customer support</span>-->
-<!--                            </h1>-->
-<!--                            <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">-->
-<!--                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.-->
-<!--                            </p>-->
-<!--                            <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">-->
-<!--                                <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">-->
-<!--                                    <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">-->
-<!--                                        Get started-->
-<!--                                    </a>-->
-<!--                                    <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8">-->
-<!--                                        Live demo-->
-<!--                                    </a>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-
-<!--            &lt;!&ndash; Logo Cloud &ndash;&gt;-->
-<!--            <div class="bg-gray-100">-->
-<!--                <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">-->
-<!--                    <p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">-->
-<!--                        Trusted by over 5 very average small businesses-->
-<!--                    </p>-->
-<!--                    <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">-->
-<!--                        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">-->
-<!--                            <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />-->
-<!--                        </div>-->
-<!--                        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">-->
-<!--                            <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />-->
-<!--                        </div>-->
-<!--                        <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">-->
-<!--                            <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />-->
-<!--                        </div>-->
-<!--                        <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">-->
-<!--                            <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor" />-->
-<!--                        </div>-->
-<!--                        <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">-->
-<!--                            <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation" />-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
+        <main>
+            <!-- Hero section -->
+            <div class="relative">
+                <div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                        <div class="absolute inset-0">
+<!--                            src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"-->
+                            <img class="h-full w-full object-cover"
+                                 src="img/IMG_0338.jpeg"
+                                 alt="Current Flyer" />
+                            <div class="absolute inset-0 bg-gradient-to-r mix-blend-multiply" />
+                        </div>
+                        <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8" style="height: 40rem;">
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 <!--            &lt;!&ndash; Alternating Feature Sections &ndash;&gt;-->
 <!--            <div class="relative pt-16 pb-32 overflow-hidden">-->
@@ -387,12 +303,12 @@
 <!--                    </div>-->
 <!--                </div>-->
 <!--            </div>-->
-<!--        </main>-->
+        </main>
 
-<!--        <footer class="bg-gray-50" aria-labelledby="footerHeading">-->
+        <footer class="bg-gray-50" aria-labelledby="footerHeading">
 <!--            <h2 id="footerHeading" class="sr-only">Footer</h2>-->
-<!--            <div class="max-w-7xl mx-auto pt-16 pb-8 px-4 sm:px-6 lg:pt-24 lg:px-8">-->
-<!--                <div class="xl:grid xl:grid-cols-3 xl:gap-8">-->
+            <div class="max-w-7xl mx-auto pt-16 pb-8 px-4 sm:px-6 lg:pt-24 lg:px-8">
+                <div class="xl:grid xl:grid-cols-3 xl:gap-8">
 <!--                    <div class="grid grid-cols-2 gap-8 xl:col-span-2">-->
 <!--                        <div class="md:grid md:grid-cols-2 md:gap-8">-->
 <!--                            <div>-->
@@ -464,20 +380,20 @@
 <!--                            </div>-->
 <!--                        </form>-->
 <!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="mt-12 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between lg:mt-16">-->
+                </div>
+                <div class="mt-12 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between lg:mt-16">
 <!--                    <div class="flex space-x-6 md:order-2">-->
 <!--                        <a v-for="item in footerNavigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">-->
 <!--                            <span class="sr-only">{{ item.name }}</span>-->
 <!--                            <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />-->
 <!--                        </a>-->
 <!--                    </div>-->
-<!--                    <p class="mt-8 text-base text-gray-400 md:mt-0 md:order-1">-->
-<!--                        &copy; 2020 Workflow, Inc. All rights reserved.-->
-<!--                    </p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </footer>-->
+                    <p class="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
+                        &copy; 2023 KPS Pools, LLC. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
