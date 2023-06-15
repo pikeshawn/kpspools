461e258cdbcab6d660a6396e1ec1085ce9895fee
Shift <shift@laravelshift.com>
2023-06-15 01:48:48 +0000

    Remove unnecessary `$model` property

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CustomerFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Customer::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
