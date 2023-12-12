452c88a2da049fb1a2adc138443851e14e1e3914
Shawn Pike <pike.shawn@gmail.com>
2023-12-11 22:08:16 -0700

    removed admin permissions to route

--- resources/js/Pages/Route/Index.vue
+++ resources/js/Pages/Route/Index.vue
@@ -13,13 +13,11 @@
         Monday
       </div>
       <div type="button"
-           v-if="user.is_admin"
            class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
            @click="getRoute('Tuesday')">
         Tuesday
       </div>
       <div type="button"
-           v-if="user.is_admin"
            class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
            @click="getRoute('Wednesday')">
         Wednesday
