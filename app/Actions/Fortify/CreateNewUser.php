53cbec49f8024ba920972745d79569ab6f4d0fbb
Shawn Pike <pike.shawn@gmail.com>
2023-12-21 17:00:53 -0700

    updated fortify create new user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,6 +3,8 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use App\Models\Customer;
+use App\Models\Address;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -17,17 +19,64 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+//        dd($input);
+
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'min_digits:10', 'max_digits:11'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
+        $phone = self::validatePhoneNumber($input['phoneNumber']);
+
+        $user = User::create([
+            'name' => $input['first_name'] . " " . $input['last_name'],
             'email' => $input['email'],
+            'type' => 'prospective',
+            'active' => 1,
+            'is_admin' => 0,
+            'phone_number' => $phone,
             'password' => Hash::make($input['password']),
         ]);
+
+        $customer = Customer::create([
+            'user_id' => $user->id,
+            'first_name' => $input['first_name'],
+            'last_name' => $input['last_name'],
+            'active' => 0,
+            'type' => 'Service',
+            'plan' => null,
+            'plan_duration' => 'Monthly',
+            'service_day' => null,
+            'plan_price' => null,
+            'chemicals_included' => 1,
+            'assigned_serviceman' => null,
+            'phone_number' => $phone,
+            'terms' => 'begin'
+        ]);
+
+        Address::create([
+            'customer_id' => $customer->id,
+            'address_line_1' => $input['first_name'],
+            'city' => $input['first_name'],
+            'state' => $input['first_name'],
+            'zip' => $input['first_name']
+        ]);
+
+        return $user;
+    }
+
+    private function validatePhoneNumber($phone): string
+    {
+        if (strlen($phone) === 10) {
+            return '1' . $phone;
+        } else if (strlen($phone) === 11){
+            return $phone;
+        }
+        return '';
     }
 }
+
