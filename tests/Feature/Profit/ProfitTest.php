cdfaf4b75c487e75dac23518677eedceb11d0856
Shawn Pike <pike.shawn@gmail.com>
2024-01-17 08:30:47 -0700

    adding profit code but is unfinished

--- tests/Feature/Profit/ProfitTest.php
+++ tests/Feature/Profit/ProfitTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Profit;
 
+use App\Models\ServiceStop;
+use App\Models\Customer;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,9 +12,35 @@ use Tests\TestCase;
 class ProfitTest extends TestCase
 {
 
+//  vendor/bin/phpunit /home/forge/kpspools.com/tests/Feature/Profit/ProfitTest.php
+
     public function test_I_can_pull_the_profit_for_each_customer_and_create_a_csv_file(): void
     {
+        // get all Users that are currently active and that have had service stops in the last year
+        $customers = ServiceStop::where('time_in', '>', '2023-01-01 00:00:00')
+            ->where('time_in', '<', '2024-01-01 00:00:00')
+            ->distinct()
+            ->pluck('customer_id');
 
+        foreach ($customers as $customerId) {
+            $customer = Customer::find($customerId);
+            if ($customer->active) {
+                $serviceStops = ServiceStop::where('customer_id', $customerId)
+                    ->where('service_type', 'Service Stop')
+                    ->where('time_in', '>', '2023-01-01 00:00:00')
+                    ->where('time_in', '<', '2024-01-01 00:00:00')
+                    ->distinct()->get();
+                $liquidChlorine = 0;
+                foreach ($serviceStops as $ss){
+                    $liquidChlorine = $liquidChlorine + $ss->liquid_chlorine;
+                }
+
+//                dd($customer->id);
+                dd($liquidChlorine);
+            }
+        }
+
+        echo $customer[0];
     }
 
 }
