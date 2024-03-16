50050c42dc18508f925e5f5ff80f661b0a6bad51
Shawn Pike <pike.shawn@gmail.com>
2024-03-15 20:04:43 -0700

    adding a constraint to prevent duplicate entries

--- database/migrations/2024_02_05_200704_add_unique_constraint_to_service_stops_table.php
+++ database/migrations/2024_02_05_200704_add_unique_constraint_to_service_stops_table.php
@@ -13,24 +13,26 @@ return new class extends Migration
     public function up(): void
     {
 
-        DB::statement('
-            DELETE t1 FROM service_stops t1
-            INNER JOIN service_stops t2
-            WHERE t1.id > t2.id
-              AND t1.user_id = t2.user_id
-              AND t1.customer_id = t2.customer_id
-              AND t1.address_id = t2.address_id
-              AND t1.time_in = t2.time_in
-              AND t1.time_out = t2.time_out
-            ');
+//        DB::statement('
+//            DELETE t1 FROM service_stops t1
+//            INNER JOIN service_stops t2
+//            WHERE t1.id > t2.id
+//              AND t1.user_id = t2.user_id
+//              AND t1.customer_id = t2.customer_id
+//              AND t1.address_id = t2.address_id
+//              AND t1.time_in = t2.time_in
+//              AND t1.time_out = t2.time_out
+//              AND t1.service_type = t2.service_type
+//            ');
 
         Schema::table('service_stops', function (Blueprint $table) {
 
-            //
+
             $table->unique([
                 'user_id',
                 'customer_id',
                 'address_id',
+                'service_type',
                 'time_in',
                 'time_out'], 'unique_service_stop');
         });
