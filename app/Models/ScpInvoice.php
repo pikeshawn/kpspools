6d8c76872aec71dbecb115397e5b2b0b1ed40258
Shawn Pike <pike.shawn@gmail.com>
2024-03-22 23:57:01 -0400

    added migrations and models for scp invoices

---
+++ app/Models/ScpInvoice.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScpInvoice extends Model
+{
+    use HasFactory;
+    protected $table = 'scp_invoice';
+    protected $guarded = [];
+
+}
---
+++ app/Models/ScpInvoiceItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScpInvoiceItem extends Model
+{
+    use HasFactory;
+    protected $table = 'scp_invoice_item';
+    protected $guarded = [];
+}
