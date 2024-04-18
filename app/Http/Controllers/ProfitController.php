9caa9e1fc43075d8e72f82d88f8c49847dea26a9
Shawn Pike <pike.shawn@gmail.com>
2024-04-17 19:47:06 -0700

    changing the profit to only unsold active accounts

--- app/Http/Controllers/ProfitController.php
+++ app/Http/Controllers/ProfitController.php
@@ -16,9 +16,9 @@ class ProfitController extends Controller
     public function index()
     {
         $totalSCPCost = ScpInvoice::sum('sales_order_amount');
-        $totalActiveCustomers = Address::count('active');
-        $totalServiceAmount = Address::where('active', 1)->sum('plan_price');
-        $totalAverageServiceAmount = Address::where('active', 1)->average('plan_price');
+        $totalActiveCustomers = Address::where('sold', false)->count('active');
+        $totalServiceAmount = Address::where('active', 1)->where('sold', false)->sum('plan_price');
+        $totalAverageServiceAmount = Address::where('active', 1)->where('sold', false)->average('plan_price');
 
         $totalAverageChemCostPerMonth = self::getTotalAverageChemCostPerMonth();
 
