32b66006bb06a9c76a713e2387fce5e2980c2158
Shawn Pike <pike.shawn@gmail.com>
2025-01-28 22:31:24 -0700

    making payment history an admin only feature

--- resources/js/Pages/Customers/Show.vue
+++ resources/js/Pages/Customers/Show.vue
@@ -500,6 +500,7 @@
                 </div>
             </inertia-link>
             <inertia-link
+                v-if="user.is_admin"
                 class="relative inline-flex items-center px-4 py-2 rounded-l-md bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
                 :href="route('payment.history', customer.id)"
             >
