afcddddc6f4aa45907715fc00b21388a1b3ff8cd
Shift <shift@laravelshift.com>
2025-03-20 17:34:35 +0000

    Convert `$casts` property to method

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,15 +76,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
      * The accessors to append to the model's array form.
      *
      * @var array
@@ -92,6 +83,18 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+        ];
+    }
 
     public function serviceStops()
     {
