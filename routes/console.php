e467831722618bd82a64ffa83304c6bda416ea6c
Shift <shift@laravelshift.com>
2025-03-20 17:29:29 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+//        Schedule::command('app:payment-reminder')->weekdays()->at('08:00');
+Schedule::command('app:task-approval-reminder')->wednesdays()->at('10:00');
